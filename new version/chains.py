99992761f6d58615566a0736eeeebad87875df72
sn0wbars <alex-borz@yandex.ru>
2017-10-12 23:20:13 +0300

    logic fix. added inertion

--- new version/chains.py
+++ new version/chains.py
@@ -15,22 +15,22 @@ class MarkovNode:
         parent.children.append(self)
 
     def __str__(self):
-        return "{}  p:{}-{}\n{}".format(self.index, self.parent.index if self.parent is not None else None, self.strongest_connect,
+        return "{} c:{} p:{}-({})\n{}".format(self.index, self.chain, self.parent.index if self.parent is not None else None, self.strongest_connect,
                                           [child.index for child in self.children])
 
 
 class MarkovChains:
     def __init__(self, max_num):
-        self.num = 0
+        self.inert = 4
         self.max_num = max_num
         self.nodes = []
         self.chains = [[] for _ in range(max_num)]
 
     def __str__(self):
-        string = "{}\n{}".format(self.num, self.chains)
+        string = str(self.chains)
         for node in self.nodes:
             string += "\n----------------------------------\n" + str(node)
-        return string + "\n----------------------------------"
+        return string + "\n----------------------------------\n"
 
     def add_node(self, prev_index, cur_index):
         log.debug("adding node {}->{}".format(prev_index, cur_index))
@@ -43,6 +43,7 @@ class MarkovChains:
                 return
         parent = self.nodes[prev_index]
         new.add_parent(parent)
+        new.strongest_connect += 1
         new.chain = parent.chain
         self.chains[new.chain].append(new.index)
 
@@ -53,8 +54,9 @@ class MarkovChains:
         prev_node = self.nodes[prev_index]
 
         # probably unpythonic as fuck
-        if new_connect > prev_node.strongest_connect:
-            self.reconnect(prev_node, cur_node, new_connect)
+        # old connections are more likely to stay
+        if new_connect > prev_node.strongest_connect + self.inert or \
+                        new_connect > cur_node.strongest_connect + self.inert:
             if prev_node.chain != cur_node.chain:
                 # older chain is more likely to absorb younger one
                 if prev_node.strongest_connect < cur_node.strongest_connect:
@@ -62,10 +64,27 @@ class MarkovChains:
                 else:
                     self.move_root(cur_node, prev_node.chain)
 
-        if new_connect > cur_node.strongest_connect:
-            self.reconnect(cur_node, prev_node, new_connect)
+            # optimized, but became less readable
+            # if new_connect > prev_node.strongest_connect:
+            #     self.reconnect(prev_node, cur_node, new_connect)
+            #
+            # if new_connect > cur_node.strongest_connect:
+            #     self.reconnect(cur_node, prev_node, new_connect)
 
-        log.debug(self.__str__())
+            if not new_connect > prev_node.strongest_connect + self.inert:
+                self.reconnect(cur_node, prev_node, new_connect)
+            else:
+                self.reconnect(prev_node, cur_node, new_connect)
+                if new_connect > cur_node.strongest_connect + self.inert:
+                    self.reconnect(cur_node, prev_node, new_connect)
+        # questionable solution, used to not allow overpopulation of chains
+        # else:
+        #     for (i, chain) in enumerate(self.chains):
+        #         if not chain:
+        #             if cur_node.parent in cur_node.children:
+        #                 log.debug("special move")
+        #                 self.move_root(cur_node, i)
+        log.debug(str(self))
 
     def reconnect(self, node, new_parent, new_connect):
         log.debug("reconnect {}->{} {}".format(node.index, new_parent.index, new_connect))
@@ -77,7 +96,7 @@ class MarkovChains:
                 if node.parent is not None:
                     node.parent.children.remove(node)
                 node.add_parent(new_parent)
-        log.debug(self.__str__())
+        log.debug(str(self))
 
     def move_root(self, node, dest_chain):
         self.move(node, dest_chain, node)
@@ -92,4 +111,4 @@ class MarkovChains:
         for child in node.children:
             if child != root:
                 self.move(child, dest_chain, root)
-        log.debug(self.__str__())
+        log.debug(str(self))
